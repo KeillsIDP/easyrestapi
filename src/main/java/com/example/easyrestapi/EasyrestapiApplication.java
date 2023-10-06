253b05096ecea5cbfe04d170e6882481226ee290
Kel <101288684+KeillsIDP@users.noreply.github.com>
2023-10-06 19:31:53 +0300

    Initial commit

---
+++ src/main/java/com/example/easyrestapi/EasyrestapiApplication.java
@@ -0,0 +1,13 @@
+package com.example.easyrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyrestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyrestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/easyrestapi/EasyrestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.easyrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyrestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
